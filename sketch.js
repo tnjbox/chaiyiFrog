9909afc0bfc20dd7f9d739359e81945c52217732
tnjbox <35487254+tnjbox@users.noreply.github.com>
2024-04-19 13:34:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://tnjbox.github.io/chaiyiFrog/model.json';
 
 
 function preload() {
